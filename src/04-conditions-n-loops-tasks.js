81d16953c97db33a6a6e40d221e507708e5cb640
Bobbbba <zwlad@tut.by>
2025-08-24 00:41:06 +0300

    feat: 54 passing

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -132,8 +132,24 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.left + rect1.width <= rect2.left) {
+    return false;
+  }
+
+  if (rect1.left >= rect2.left + rect2.width) {
+    return false;
+  }
+
+  if (rect1.top + rect1.height <= rect2.top) {
+    return false;
+  }
+
+  if (rect1.top >= rect2.top + rect2.height) {
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -162,7 +178,7 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
+function isInsideCircle(circle, point) {
   throw new Error('Not implemented');
 }
 
